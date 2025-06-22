773a886e544f80e9af9f9860fe325dcd6545f7f4
Farida Salakhova <ruzilya.a.s@icloud.com>
2025-06-22 10:38:47 +0300

    внесены изменения

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
 const init = () => {
     headerModal()
+    openModal ()
     swiper()
     headerBcc()
+    initCartModal(); 
 }
 
 
@@ -39,8 +41,7 @@ const swiper = () => {
         spaceBetween: 30,
         centeredSlides: true,
         autoplay: {
-            // delay: 2500,
-            delay: 2500000000000,
+            delay: 2500,
             disableOnInteraction: false,
         },
         pagination: {
@@ -73,3 +74,85 @@ const headerBcc = () => {
 
 }
 document.addEventListener('DOMContentLoaded', init);
+
+function openModal() {
+    const overlayModal = document.querySelector('.popup-overlay')
+    const bigImageModal = document.querySelector('.popup-image')
+    console.log(bigImageModal);
+
+    
+    const triggerModals = document.querySelectorAll('.new-card')
+    
+
+    triggerModals.forEach(triggerModal => {
+        triggerModal.addEventListener("click", () => {
+            const imageModal = triggerModal.querySelector('img').src
+            console.log(imageModal);
+            bigImageModal.src = imageModal
+            console.log(bigImageModal);
+            overlayModal.style.display = 'flex'
+            document.body.style.overflow = 'hidden'
+        })
+
+        const closeModal = document.querySelector('.popup-close')
+        if (!closeModal) return false
+
+        closeModal.addEventListener('click', () => {
+            overlayModal.style.display = 'none'
+            document.body.style.overflow = 'scroll'
+        })
+
+        overlayModal.addEventListener("click", (event) => {
+            if (event.target === overlayModal) {
+                overlayModal.style.display = 'none'
+                document.body.style.overflow = 'scroll'
+            }
+        })
+
+    });
+}
+
+const initCartModal = () => {
+    // ищет иконку корзины во всем доке
+    const cartIcon = document.querySelector('.fa-basket-shopping'); 
+    // ищет фон модалки, чтобы ткнуть и закрылось
+    const cartOverlay = document.querySelector('.cart-overlay');
+    // ищет крестик
+    const cartClose = document.querySelector('.cart-close');
+    
+    // проверяем все ли элементы существуют, если нет, завершаем работу
+    if (!cartIcon || !cartOverlay || !cartClose) return;
+    
+
+    // обработчик клика на иконку корины 
+    cartIcon.addEventListener('click', (e) => {
+
+        // епревент дефолт это метод объекта события event, который убирает стандартное поведение, т е иконка корзины у меня ссылка а, но мне не нужно по ней переходить, перезагружать страницу и тд. поэтому я взяла и отменила эт все 
+        e.preventDefault();
+        // не дает странице листаться пока окно открыто 
+        document.body.style.overflow = 'hidden';
+        // делает оверфлоу видимым (изначально он скрыт) 
+        cartOverlay.style.display = 'block';
+        // добавляет с задержкой 10 миллисек класс актив (он в ссс) чтобы анимация была плавной 
+        setTimeout(() => {
+            cartOverlay.classList.add('active');
+        }, 10);
+    });
+    
+    // Зачем нужна е в параметре?  это параметр функции-обработчика, который содержит объект события, как раз для превентдефолта и нужен. 
+    
+    // закрывает все при клике на крестик, снова можно листать, а модалку не видно 
+    cartClose.addEventListener('click', () => {
+        cartOverlay.style.display = 'none';
+        document.body.style.overflow = 'scroll';
+    });
+
+    // закрывает все при клике вне модалки
+    cartOverlay.addEventListener('click', (e) => {
+        if (e.target === cartOverlay) {
+            cartOverlay.style.display = 'none';
+            document.body.style.overflow = 'scroll';
+        }
+    });
+};
+
